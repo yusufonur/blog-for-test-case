974850b14db5eb32c33a0951a4bc6cd19689739b
Yusuf Onur SARI <15554011@firat.edu.tr>
2020-10-31 13:26:58 +0300

    Fixed

---
+++ src/Api/V1/Users/DataTransferObjects/UserData.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Api\V1\Users\DataTransferObject;
+
+
+use Api\V1\Users\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class UserData extends DataTransferObject
+{
+    public string $name;
+
+    public string $email;
+
+    public ?string $password;
+
+
+    public static function fromRequest(Request $request): UserData
+    {
+        $data = [
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => null
+        ];
+
+        if ($request->input("password")) {
+            $data["password"] = Hash::make($request->password);
+        }
+
+        return new self($data);
+    }
+}
